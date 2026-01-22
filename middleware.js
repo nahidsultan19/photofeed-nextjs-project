682bc3121bd29521e9678fecba1fc70304bbb37c
Nahid Sultan <nahidsultan38@gmail.com>
2026-01-22 16:10:53 +0800

    implement internalization for language.

---
+++ middleware.js
@@ -0,0 +1,35 @@
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse } from "next/server";
+
+let locales = ["en", "bn"];
+let defaultLocale = "en";
+
+function getLocale(request) {
+  const acceptedLanguage = request.headers.get("accept-language") ?? undefined;
+
+  let headers = { "accept-language": acceptedLanguage };
+  const languages = new Negotiator({ headers }).languages();
+
+  return match(languages, locales, defaultLocale);
+}
+
+export function middleware(request) {
+  const pathname = request.nextUrl.pathname;
+
+  const pathnameIsMissingLocale = locales.every(
+    (locale) =>
+      !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+  );
+
+  if (pathnameIsMissingLocale) {
+    const locale = getLocale(request);
+
+    return NextResponse.redirect(new URL(`/${locale}${pathname}`, request.url));
+  }
+}
+
+export const config = {
+  // skip all internal paths like api, _next, assets,
+  matcher: ["/((?!api|assets|.*\\..*|_next).*)"],
+};
